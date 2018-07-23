7e51ea6949515db5c43d0a86c6e700b3f8a5aa46
rtlopez <rtlopezdev@gmail.com>
2018-07-23 15:52:04 +0200

    hal refactor

--- lib/Espfc/src/Hardware.h
+++ lib/Espfc/src/Hardware.h
@@ -23,6 +23,23 @@
 #include "Device/MagHMC5338L.h"
 
 namespace {
+#if defined(ESP32)
+  static HardwareSerial Serial1(1);
+  static HardwareSerial Serial2(2);
+  static Espfc::SerialDeviceAdapter<HardwareSerial> uart0(Serial);
+  static Espfc::SerialDeviceAdapter<HardwareSerial> uart1(Serial1);
+  static Espfc::SerialDeviceAdapter<HardwareSerial> uart2(Serial2);
+#endif
+#if defined(ESP8266)
+#if defined(USE_SOFT_SERIAL)
+  static EspSoftSerial softSerial;
+  static Espfc::SerialDeviceAdapter<EspSoftSerial>  soft0(softSerial);
+#endif
+  static Espfc::SerialDeviceAdapter<HardwareSerial> uart0(Serial);
+  static Espfc::SerialDeviceAdapter<HardwareSerial> uart1(Serial1);
+#endif
+  static Espfc::InputPPM ppm;
+  static Espfc::InputSBUS sbus;
   static Espfc::Device::BusSPI spiBus;
   static Espfc::Device::BusI2C i2cBus;
   static Espfc::Device::GyroMPU6050 mpu6050;
@@ -166,8 +183,10 @@ class Hardware
           sc.stop_bits = SERIAL_STOP_BITS_2;
           sc.parity = SERIAL_PARITY_EVEN;
           sc.inverted = true;
+          #if defined(ESP8266)
           sc.rx_pin = _model.config.pin[PIN_INPUT_RX];
-          serial->flush();
+          #endif
+          //serial->flush();
           serial->begin(sc);
           _model.logger.info().log(F("UART")).log(i).log(sc.baud).log(sc.inverted).logln(F("sbus"));
         }
@@ -175,7 +194,7 @@ class Hardware
         {
           int idx = _model.config.serial[i].blackboxBaudIndex == SERIAL_SPEED_INDEX_AUTO ? _model.config.serial[i].baudIndex : _model.config.serial[i].blackboxBaudIndex;
           sc.baud = fromIndex((SerialSpeedIndex)idx, SERIAL_SPEED_115200);
-          serial->flush();
+          //serial->flush();
           serial->begin(sc);
           _model.logger.info().log(F("UART")).log(i).log(sc.baud).log(sc.inverted).log(F("msp")).logln(F("blackbox"));
         }
@@ -183,20 +202,20 @@ class Hardware
         {
           int idx = _model.config.serial[i].blackboxBaudIndex == SERIAL_SPEED_INDEX_AUTO ? _model.config.serial[i].baudIndex : _model.config.serial[i].blackboxBaudIndex;
           sc.baud = fromIndex((SerialSpeedIndex)idx, SERIAL_SPEED_250000);
-          serial->flush();
+          //serial->flush();
           serial->begin(sc);
           _model.logger.info().log(F("UART")).log(i).log(sc.baud).log(sc.inverted).logln(F("blackbox"));
         }
         else if(msp || deb)
         {
           sc.baud = fromIndex((SerialSpeedIndex)_model.config.serial[i].baudIndex, SERIAL_SPEED_115200);
-          serial->flush();
+          //serial->flush();
           serial->begin(sc);
           _model.logger.info().log(F("UART")).log(i).log(sc.baud).log(msp ? F("msp") : F("")).logln(deb ? F("debug") : F(""));
         }
         else
         {
-          serial->flush();
+          //serial->flush();
           _model.logger.info().log(F("UART")).log(i).logln(F("free"));
         }
       }
@@ -236,22 +255,6 @@ class Hardware
 
     static SerialDevice * getSerialPortById(SerialPort portId)
     {
-#if defined(ESP32)
-      static HardwareSerial Serial1(1);
-      static HardwareSerial Serial2(2);
-      static SerialDeviceAdapter<HardwareSerial> uart0(Serial);
-      static SerialDeviceAdapter<HardwareSerial> uart1(Serial1);
-      static SerialDeviceAdapter<HardwareSerial> uart2(Serial2);
-#endif
-#if defined(ESP8266)
-  #if defined(USE_SOFT_SERIAL)
-      static EspSoftSerial softSerial;
-      static SerialDeviceAdapter<EspSoftSerial>  soft0(softSerial);
-  #endif
-      static SerialDeviceAdapter<HardwareSerial> uart0(Serial);
-      static SerialDeviceAdapter<HardwareSerial> uart1(Serial1);
-#endif
-
       switch(portId)
       {
         case SERIAL_UART_0: return &uart0;
@@ -267,7 +270,6 @@ class Hardware
 
     static Device::GyroDevice * getGyroDevice(const Model& model)
     {
-      //return &mpu6050;
       return detectedGyro;
     }
 
@@ -278,9 +280,6 @@ class Hardware
 
     static InputDevice * getInputDevice(Model& model)
     {
-      static InputPPM ppm;
-      static InputSBUS sbus;
-
       SerialDevice * serial = getSerialPort(model.config.serial, SERIAL_FUNCTION_RX_SERIAL);
       if(serial)
       {
