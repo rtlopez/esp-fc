d269b7ff1bb567d4ea8efae7a003354c5faf069a
rtlopez <rtlopezdev@gmail.com>
2020-06-26 23:48:49 +0200

    fixes #10 - esp32 disable core0 wdt

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,14 @@
 #if defined(ESP32)
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+//#include "esp_task_wdt.h"
 #include <WiFi.h>
-#if !CONFIG_FREERTOS_UNICORE
-#define ESPFC_DUAL_CORE 1
-#endif
+  #if !CONFIG_FREERTOS_UNICORE
+    #define ESPFC_DUAL_CORE 1
+    TaskHandle_t otherTaskHandle = NULL;
+  #endif
 #elif defined(ESP8266)
-#include <ESP8266WiFi.h>
+  #include <ESP8266WiFi.h>
 #endif
 
 Espfc::Espfc espfc;
@@ -29,7 +31,12 @@ Espfc::Espfc espfc;
 #if defined(ESPFC_DUAL_CORE)
 void otherTask(void *pvParameters)
 {
-  while(true) espfc.updateOther();
+  //esp_task_wdt_add(otherTaskHandle);
+  while(true)
+  {
+    //esp_task_wdt_reset();
+    espfc.updateOther();
+  }
 }
 #endif
 
@@ -37,7 +44,8 @@ void setup()
 {
   espfc.begin();
   #if defined(ESPFC_DUAL_CORE)
-  xTaskCreatePinnedToCore(otherTask, "otherTask", 8192, NULL, 1, NULL, 0); // run on PRO(0) core, loopTask is on APP(1)
+  disableCore0WDT();
+  xTaskCreatePinnedToCore(otherTask, "otherTask", 8192, NULL, 1, &otherTaskHandle, 0); // run on PRO(0) core, loopTask is on APP(1)
   #endif
 }
 
