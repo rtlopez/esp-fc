a3958ed8192e38b4d326e1ae4665666f71e1f550
rtlopez <rtlopezdev@gmail.com>
2023-07-01 01:12:40 +0200

    build fix

--- lib/Espfc/src/Device/SerialDevice.h
+++ lib/Espfc/src/Device/SerialDevice.h
@@ -136,7 +136,7 @@ class SerialDevice: public Stream
     virtual int peek() = 0;
     virtual void flush() = 0;
     virtual size_t write(uint8_t c) = 0;
-    virtual size_t write(uint8_t * c, size_t l) = 0;
+    virtual size_t write(const uint8_t * c, size_t l) = 0;
     virtual int availableForWrite() = 0;
     virtual bool isTxFifoEmpty() = 0;
     virtual bool isSoft() const = 0;
