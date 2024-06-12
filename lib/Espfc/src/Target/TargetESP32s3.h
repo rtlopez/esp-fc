193c79bdfa625121ba9e95c9badc1043843b8413
thomascatlee <thomascatlee@gmail.com>
2024-06-12 19:44:03 +0800

    modified Pins definition for pyDrone hardware

--- lib/Espfc/src/Target/TargetESP32s3.h
+++ lib/Espfc/src/Target/TargetESP32s3.h
@@ -9,13 +9,13 @@
 // usb/jtag: 19, 20
 
 #define ESPFC_INPUT
-#define ESPFC_INPUT_PIN 6 // ppm
+#define ESPFC_INPUT_PIN -1 // ppm
 
 #define ESPFC_OUTPUT_COUNT 4
-#define ESPFC_OUTPUT_0 39
-#define ESPFC_OUTPUT_1 40
-#define ESPFC_OUTPUT_2 41
-#define ESPFC_OUTPUT_3 42
+#define ESPFC_OUTPUT_0 5
+#define ESPFC_OUTPUT_1 4
+#define ESPFC_OUTPUT_2 40
+#define ESPFC_OUTPUT_3 41
 
 #define ESPFC_SERIAL_0
 #define ESPFC_SERIAL_0_DEV Serial0
@@ -26,7 +26,7 @@
 #define ESPFC_SERIAL_0_BAUD (SERIAL_SPEED_115200)
 #define ESPFC_SERIAL_0_BBAUD (SERIAL_SPEED_NONE)
 
-#define ESPFC_SERIAL_1
+//#define ESPFC_SERIAL_1
 #define ESPFC_SERIAL_1_DEV Serial1
 #define ESPFC_SERIAL_1_DEV_T HardwareSerial
 #define ESPFC_SERIAL_1_TX 16
@@ -35,7 +35,7 @@
 #define ESPFC_SERIAL_1_BAUD (SERIAL_SPEED_115200)
 #define ESPFC_SERIAL_1_BBAUD (SERIAL_SPEED_NONE)
 
-#define ESPFC_SERIAL_2
+//#define ESPFC_SERIAL_2
 #define ESPFC_SERIAL_2_DEV Serial2
 #define ESPFC_SERIAL_2_DEV_T HardwareSerial
 #define ESPFC_SERIAL_2_TX 18
@@ -57,7 +57,7 @@
 #define ESPFC_SERIAL_DEBUG_PORT SERIAL_USB
 #define SERIAL_TX_FIFO_SIZE 0xFF
 
-#define ESPFC_SPI_0
+//#define ESPFC_SPI_0
 #define ESPFC_SPI_0_DEV SPI1
 #define ESPFC_SPI_0_SCK 12
 #define ESPFC_SPI_0_MOSI 11
@@ -67,17 +67,17 @@
 #define ESPFC_SPI_CS_BARO 7
 
 #define ESPFC_I2C_0
-#define ESPFC_I2C_0_SCL 10
-#define ESPFC_I2C_0_SDA 9
+#define ESPFC_I2C_0_SCL 15
+#define ESPFC_I2C_0_SDA 16
 #define ESPFC_I2C_0_SOFT
 
-#define ESPFC_BUZZER
+//#define ESPFC_BUZZER
 #define ESPFC_BUZZER_PIN 5
 
 #define ESPFC_ADC_0
-#define ESPFC_ADC_0_PIN 1
+#define ESPFC_ADC_0_PIN 2
 
-#define ESPFC_ADC_1
+//#define ESPFC_ADC_1
 #define ESPFC_ADC_1_PIN 4
 
 #define ESPFC_ADC_SCALE (3.3f / 4096)
