6abae4a601b714807638ac43b92de104dc46c7b0
rtlopez <rtlopezdev@gmail.com>
2024-10-31 22:54:42 +0100

    split baro, mag and voltage sensors

--- lib/Espfc/src/Sensor/BaroSensor.h
+++ lib/Espfc/src/Sensor/BaroSensor.h
@@ -2,6 +2,7 @@
 #define _ESPFC_SENSOR_BARO_SENSOR_H_
 
 #include "BaseSensor.h"
+#include "Model.h"
 #include "Device/BaroDevice.h"
 #include "Filter.h"
 
@@ -19,122 +20,16 @@ class BaroSensor: public BaseSensor
       BARO_STATE_PRESS_GET,
     };
 
-    BaroSensor(Model& model): _model(model), _state(BARO_STATE_INIT), _counter(0) {}
+    BaroSensor(Model& model);
 
-    int begin()
-    {
-      _model.state.baro.rate = 0;
-      if(!_model.baroActive()) return 0;
-      _baro = _model.state.baro.dev;
-      if(!_baro) return 0;
-
-      _baro->setMode(BARO_MODE_TEMP);
-      int delay = _baro->getDelay(BARO_MODE_TEMP) + _baro->getDelay(BARO_MODE_PRESS);
-      int toGyroRate = (delay / _model.state.gyro.timer.interval) + 1; // number of gyro readings per cycle
-      int interval = _model.state.gyro.timer.interval * toGyroRate;
-      _model.state.baro.rate = 1000000 / interval;
-
-      _model.state.baro.altitudeBiasSamples = 2 * _model.state.baro.rate;
-
-      // TODO: move filters to BaroState
-      _temperatureFilter.begin(FilterConfig(FILTER_PT1, _model.state.baro.rate * 0.1f), _model.state.baro.rate);
-      _pressureFilter.begin(FilterConfig(FILTER_PT1, _model.state.baro.rate * 0.1f), _model.state.baro.rate);
-      _altitudeFilter.begin(_model.config.baro.filter, _model.state.baro.rate);
-
-      _model.logger.info().log(F("BARO INIT")).log(FPSTR(Device::BaroDevice::getName(_baro->getType()))).log(_baro->getAddress()).log(toGyroRate).log(_model.state.baro.rate).logln(_model.config.baro.filter.freq);
-
-      return 1;
-    }
-
-    int update()
-    {
-      int status = read();
-
-      return status;
-    }
-
-    int read()
-    {
-      if(!_baro || !_model.baroActive()) return 0;
-      
-      if(_wait > micros()) return 0;
-
-      Stats::Measure measure(_model.state.stats, COUNTER_BARO);
-
-      if(_model.config.debug.mode == DEBUG_BARO)
-      {
-        _model.state.debug[0] = _state;
-      }
-
-      switch(_state)
-      {
-        case BARO_STATE_INIT:
-          _baro->setMode(BARO_MODE_TEMP);
-          _state = BARO_STATE_TEMP_GET;
-          _wait = micros() + _baro->getDelay(BARO_MODE_TEMP);
-          return 0;
-        case BARO_STATE_TEMP_GET:
-          readTemperature();
-          _baro->setMode(BARO_MODE_PRESS);
-          _state = BARO_STATE_PRESS_GET;
-          _wait = micros() + _baro->getDelay(BARO_MODE_PRESS);
-          _counter = 1;
-          return 1;
-        case BARO_STATE_PRESS_GET:
-          readPressure();
-          updateAltitude();
-          if(--_counter > 0)
-          {
-            _baro->setMode(BARO_MODE_PRESS);
-            _state = BARO_STATE_PRESS_GET;
-            _wait = micros() + _baro->getDelay(BARO_MODE_PRESS);
-          }
-          else
-          {
-            _baro->setMode(BARO_MODE_TEMP);
-            _state = BARO_STATE_TEMP_GET;
-            _wait = micros() + _baro->getDelay(BARO_MODE_TEMP);
-          }
-          return 1;
-          break;
-        default:
-          _state = BARO_STATE_INIT;
-          break;
-      }
-
-      return 0;
-    }
+    int begin();
+    int update();
+    int read();
 
   private:
-    void readTemperature()
-    {
-      _model.state.baro.temperatureRaw = _baro->readTemperature();
-      _model.state.baro.temperature = _temperatureFilter.update(_model.state.baro.temperatureRaw);
-    }
-
-    void readPressure()
-    {
-      _model.state.baro.pressureRaw = _baro->readPressure();
-      _model.state.baro.pressure = _pressureFilter.update(_model.state.baro.pressureRaw);
-    }
-
-    void updateAltitude()
-    {
-      _model.state.baro.altitudeRaw = _altitudeFilter.update(Math::toAltitude(_model.state.baro.pressure));
-      if(_model.state.baro.altitudeBiasSamples > 0)
-      {
-        _model.state.baro.altitudeBiasSamples--;
-        _model.state.baro.altitudeBias += (_model.state.baro.altitudeRaw - _model.state.baro.altitudeBias) * 0.2f;
-      }
-      _model.state.baro.altitude = _model.state.baro.altitudeRaw - _model.state.baro.altitudeBias;
-
-      if(_model.config.debug.mode == DEBUG_BARO)
-      {
-        _model.state.debug[1] = lrintf(_model.state.baro.pressureRaw * 0.1f); // hPa x 10
-        _model.state.debug[2] = lrintf(_model.state.baro.temperatureRaw * 100.f); // deg C x 100
-        _model.state.debug[3] = lrintf(_model.state.baro.altitudeRaw * 10.f);
-      }
-    }
+    void readTemperature();
+    void readPressure();
+    void updateAltitude();
 
     Model& _model;
     Device::BaroDevice * _baro;
